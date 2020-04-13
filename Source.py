5a80afcab6de5fc05a077d67297961473b48e56e
casboomdtp@gmail.com <casboomdtp@gmail.com>
2020-04-13 11:40:34 +0200

    add base classes

---
+++ Source.py
@@ -0,0 +1,3 @@
+class Apa_source:
+    def __construct__(self):
+        pass
---
+++ sourcerer.py
@@ -0,0 +1,9 @@
+import Start as st
+import Source as s
+
+start = st.Start()
+start.render()
+
+running = True
+while(running):
+    running = False
